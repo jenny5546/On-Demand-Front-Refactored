b535ac0bdd1e78cfe052864edf7b72606111451c
jenny5546 <jenny5546@naver.com>
2020-01-23 12:44:09 +0900

    model 구조

--- adminpage/models.py
+++ adminpage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+
+class Request(models.Model)
+
+class Theme(models.Model)
+
+class FloorPlan(models.Model)
+
--- ondemand/urls.py
+++ ondemand/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 import adminpage.views
+from django.conf.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', adminpage.views.index, name='index'),
+    path('admin/', include('adminpage.urls')),
 ]
