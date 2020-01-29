ff5f36670b2bcbf49256518496b257d915e83fa0
jenny5546 <jenny5546@naver.com>
2020-01-29 11:47:37 +0900

    자잘한거 수정

--- ondemand/settings.py
+++ ondemand/settings.py
@@ -156,6 +156,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'sass_processor.finders.CssFinder',
 ]
