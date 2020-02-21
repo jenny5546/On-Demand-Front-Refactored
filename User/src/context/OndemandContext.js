47f8156c822ac8aa06378bf8c53d8a1604d43b03
jenny5546 <jenny5546@naver.com>
2020-02-21 17:13:56 +0900

    주석 지움

--- User/src/context/OndemandContext.js
+++ User/src/context/OndemandContext.js
@@ -5,13 +5,9 @@ const OndemandContext = createContext();
 
 const { Provider, Consumer: OndemandConsumer } = OndemandContext;
 
-//Store 전체 관리 
 // Provider 에서 state 를 사용하기 위해서 컴포넌트를 새로 만들어줍니다.
 
 class OndemandProvider extends Component {
-
-    //진짜 state 는 하나, openModal(Boolean)
-    //그 state를 바꾸는 handleOpenModal도 Provider에서 value로 넘겨준다. 
 
     state = {
         openModal: false
@@ -32,19 +28,6 @@ class OndemandProvider extends Component {
                 }}
             >
                 {this.props.children} 
-
-                {/* 
-                    참고: 
-                    
-                    The React docs say that you can use props.children 
-                    on components that represent ‘generic boxes’ and 
-                    that ‘don’t know their children ahead of time’.
-
-                    what {this.props.children} does is that it is used to display 
-                    whatever you include between the opening and closing tags 
-                    when invoking a component. 
-
-                */}
 
             </Provider>
         );
