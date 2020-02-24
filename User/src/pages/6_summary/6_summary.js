2850d89c92ac5abd01f14eb360d458c4b4cf8292
jenny5546 <jenny5546@naver.com>
2020-02-24 16:33:36 +0900

    back 연결 준비완료

--- User/src/pages/6_summary/6_summary.js
+++ User/src/pages/6_summary/6_summary.js
@@ -18,11 +18,16 @@ const Summary = props => {
 
     const contextType = useContext(OndemandContext);
     
-    const handleNextStep = e => {
+    const handleConfirm = e => {
         //얘는 마지막 단계니까 payment 로 이동하게 바꾸기
         e.preventDefault();
-        contextType.handleSubmit(); //POST
-        props.nextStep();
+        contextType.handleSubmit(); //ADMIN PAGE로 포스트 하는 부분 
+
+
+        /* *-------------------------------------------------------------------* 
+                    이 부분은 앞으로 결제 창으로 넘어가도록 바꾸어야 하는 부분 
+        *-------------------------------------------------------------------* */
+        window.location.replace("http://localhost:3000");
     };
     const handleBtnBack = e => {
         e.preventDefault();
@@ -257,7 +262,7 @@ const Summary = props => {
                                     Edit Application
                                 </BtnBottom>
 
-                                <BtnBottom onClick={handleNextStep}>
+                                <BtnBottom onClick={handleConfirm}>
                                     Confirm Payment
                                 </BtnBottom>
                             </section>
